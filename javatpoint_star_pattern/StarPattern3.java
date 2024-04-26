ba956e46975ff9a73eabb129dd6f7726d40f6ba9
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-04-26 11:23:30 +0530

    Add files via upload

---
+++ javatpoint_star_pattern/StarPattern3.java
@@ -0,0 +1,18 @@
+package javatpoint_StarPattern;
+
+public class StarPattern3 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++) {
+			
+			for(int j=5; j>=i;j--) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+}

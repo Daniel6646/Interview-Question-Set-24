984cf754149e0eda2c1f5b9f754d23ac7ee3e408
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-05-03 20:37:45 +0530

    Update SmithNumber.java

--- javatpoint_number_program/SmithNumber.java
+++ javatpoint_number_program/SmithNumber.java
@@ -4,6 +4,30 @@ import java.util.Scanner;
 
 public class SmithNumber {
 
+//	A Smith number is a composite number whose sum of digits equals to the sum of digits of its prime factors, excluding 1. It is also known as a joke number
+//	
+//	Example 1: Suppose, we want to check the number 85 is smith or not.
+//	Sum of digits = 8 + 5 = 13
+//
+//	Prime factors of 85 is: 5,17
+//
+//	Sum of digits of its prime factors = 5 + 1+ 7 = 13
+//
+//	Compare the sum of digits with the sum of digits of its prime factors i.e. 13=13. Both are equal. Hence, the given number (85) is a smith number.
+//
+//	Example 2: Let's check another number 999 is smith or not.
+//
+//	Sum of digits = 9+ 9+9 = 27
+//	
+//			Prime factors of 999 is: 3×3×3,37
+//
+//			Sum of digits of its prime factors = 3+3+3+3+7 =19
+//
+//			Compare the sum of digits with the sum of digits of its prime factors i.e. 27≠19. Hence, the given number (999) is not a smith number.
+//	
+
+
+	
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a number to check if its smith number or not: ");

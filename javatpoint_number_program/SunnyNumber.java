48aea84ba283dd9e73ad87e348ab8a43203263bd
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-05-03 21:04:42 +0530

    Update SunnyNumber.java

--- javatpoint_number_program/SunnyNumber.java
+++ javatpoint_number_program/SunnyNumber.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class SunnyNumber {
 
 	//A number is called a sunny number if the number next to the given number is a perfect square. In other words, a number N will be a sunny number if N+1 is a perfect square.
+//	Given, N=80 then N+1 will be 80+1=81, which is a perfect square of the number 9. Hence 80 is a sunny number.
+//
+//			Let's take another number 10.
+//
+//			Given, N=10 then N+1 will be 10+1=11, which is not a perfect square. Hence 10 is not a sunny number.
+
 	
 	public static void main(String[] args) {
 		

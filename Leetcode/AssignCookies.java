270ff6f63edc648d4d557ab4835c6306931b0035
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-05-06 20:53:15 +0530

    Update AssignCookies.java

--- Leetcode/AssignCookies.java
+++ Leetcode/AssignCookies.java
@@ -85,5 +85,24 @@ public class AssignCookies {
 		   
 		 return maxNum;  
 	    }
+
+
+
+public int findContentChildren2(int[] g, int[] s) {
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int childIndex = 0;
+        int cookieIndex = 0;
+        while (cookieIndex < s.length && childIndex < g.length) {
+
+            if (g[childIndex] <= s[cookieIndex]) { childIndex++; }
+            cookieIndex++;
+        }    
+
+        return childIndex; 
+    }
+
 	
 }

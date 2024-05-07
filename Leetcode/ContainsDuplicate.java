a0db472c018e4080e76d7931d6251f87f075e2c9
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-05-07 18:46:15 +0530

    Update ContainsDuplicate.java

--- Leetcode/ContainsDuplicate.java
+++ Leetcode/ContainsDuplicate.java
@@ -4,6 +4,30 @@ import java.util.Arrays;
 
 public class ContainsDuplicate {
 
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+	
 	public static void main(String[] args) {
 		
 		int nums1 [] = new int [] {1,2,3,1};

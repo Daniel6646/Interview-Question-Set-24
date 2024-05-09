6a6f7d7c4089f6bf873c1e128c4875f9b8549e45
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-05-09 17:24:39 +0530

    Update FizzBuzz.java

--- Leetcode/FizzBuzz.java
+++ Leetcode/FizzBuzz.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 public class FizzBuzz {
 
+
+//	Given an integer n, return a string array answer (1-indexed) where:
+//
+//		answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+//		answer[i] == "Fizz" if i is divisible by 3.
+//		answer[i] == "Buzz" if i is divisible by 5.
+//		answer[i] == i (as a string) if none of the above conditions are true.
+//		 
+	
 //	Example 1:
 //
 //		Input: n = 3

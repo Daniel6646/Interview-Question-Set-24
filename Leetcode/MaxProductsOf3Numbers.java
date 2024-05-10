2f75657847ca38d880b92d460890906c8d1e03ec
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-05-10 17:29:07 +0530

    Update MaxProductsOf3Numbers.java

--- Leetcode/MaxProductsOf3Numbers.java
+++ Leetcode/MaxProductsOf3Numbers.java
@@ -4,6 +4,30 @@ import java.util.Arrays;
 
 public class MaxProductsOf3Numbers {
 
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: 6
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: 24
+// Example 3:
+
+// Input: nums = [-1,-2,-3]
+// Output: -6
+ 
+
+// Constraints:
+
+// 3 <= nums.length <= 104
+// -1000 <= nums[i] <= 1000
+
+	
 	public int maximumProduct(int nums[]) {
 		
 		int product = 1;

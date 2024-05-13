9a4c64f4fd3a82c9b31903474dbfdacd66ccfda9
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-05-13 18:21:01 +0530

    Update RemoveDuplicateFromSQL.java

--- Leetcode/RemoveDuplicateFromSQL.java
+++ Leetcode/RemoveDuplicateFromSQL.java
@@ -53,5 +53,37 @@ The result format is in the following example.
 //where p1.email = p2.email
 // and p1.id > p2.id;
 // 
+
+
+// Explanation:
+
+// Input: 
+// Person table:
+// +----+------------------+
+// | id | email            |
+// +----+------------------+
+// | 1  | john@example.com |
+// | 2  | bob@example.com  |
+// | 3  | john@example.com |
+// +----+------------------+
+
+// => From Person p1, Person p2 : it would look like:
+// 1|john@example.com|1|john@example.com
+// 1|john@example.com|2|bob@example.com 
+// 1|john@example.com|3|john@example.com
+
+// 2|bob@example.com|1|john@example.com
+// 2|bob@example.com|2|bob@example.com
+// 2|bob@example.com|3|john@example.com
+
+// 3|john@example.com|1|john@example.com
+// 3|john@example.com|2|bob@example.com
+// 3|john@example.com|3|john@example.com
+
+// => From Person p1, Person p2 where p1.email=p2.email and p1.id>p2.id:
+// It would look like:
+// 3|john@example.com|1|john@example.com
+
+// Now delete this row's matching row in p1 using p1:  delete p1
 	
 }

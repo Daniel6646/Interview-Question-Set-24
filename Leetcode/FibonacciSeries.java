8b7c6db71d4eef23dc9723e752eb21d60d66e8ed
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-07-15 12:36:17 +0530

    Update FibonacciSeries.java

--- Leetcode/FibonacciSeries.java
+++ Leetcode/FibonacciSeries.java
@@ -34,9 +34,14 @@ public class FibonacciSeries {
 //	0 <= n <= 30
 
 	
+	 public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
 	
-	
-	public int fib(int n) {
+	public int fib2(int n) {
 
 		if (n <= 1) {
 

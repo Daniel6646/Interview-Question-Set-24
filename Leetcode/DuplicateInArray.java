cc419fb9179b4b8bbeb5ab827854997db396fdbb
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2024-07-15 12:01:05 +0530

    Update DuplicateInArray.java

--- Leetcode/DuplicateInArray.java
+++ Leetcode/DuplicateInArray.java
@@ -25,6 +25,20 @@ public class DuplicateInArray {
 //		Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 //		It does not matter what you leave beyond the returned k (hence they are underscores).
 //	
+
+	
+// The code starts iterating from i = 1 because we need to compare each element with its previous element to check for duplicates.
+
+// The main logic is inside the for loop:
+
+// If the current element nums[i] is not equal to the previous element nums[i - 1], it means we have encountered a new unique element.
+// In that case, we update nums[j] with the value of the unique element at nums[i], and then increment j by 1 to mark the next position for a new unique element.
+// By doing this, we effectively overwrite any duplicates in the array and only keep the unique elements.
+// Once the loop finishes, the value of j represents the length of the resulting array with duplicates removed.
+
+// Finally, we return j as the desired result.	
+	
+	
 	public  int removeDuplicates(int [] nums) {
         int j = 1;
         for(int i = 1; i < nums.length; i++){

decd25f5a09267659a52b4d4645c5d22f100f925
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2026-04-27 16:37:09 +0530

    Update ReverseString2.java

--- Leetcode/ReverseString2.java
+++ Leetcode/ReverseString2.java
@@ -46,4 +46,35 @@ public class ReverseString2 {
 	
 	    	}
 
+
+	
+	    public String reverseStr2(String s, int k) {
+
+	        char[] arr = s.toCharArray();
+	        int n = arr.length;
+
+	        for (int i = 0; i < n; i += 2 * k) {
+
+	            int left = i;
+
+	            // take min to avoid going out of bounds
+	            int right = Math.min(i + k - 1, n - 1);
+
+	            // reverse from left to right
+	            while (left < right) {
+	                char temp = arr[left];
+	                arr[left] = arr[right];
+	                arr[right] = temp;
+	                left++;
+	                right--;
+	            }
+	        }
+
+	        return new String(arr);
+	    } 
+	    
+	    
+
+
+	
 }

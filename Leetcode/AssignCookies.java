38074812d5f371fcbb3a66257a83f0f42ab95c83
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2026-07-21 18:35:02 +0530

    Update AssignCookies.java

--- Leetcode/AssignCookies.java
+++ Leetcode/AssignCookies.java
@@ -18,6 +18,7 @@ public class AssignCookies {
 //	Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
 //	And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
 //	You need to output 1.
+
 //	Example 2:
 //
 //	Input: g = [1,2], s = [1,2,3]
@@ -85,27 +86,27 @@ public class AssignCookies {
 		   
 		 return maxNum;  
 	    }
-
-
-
-public int findContentChildren2(int[] g, int[] s) {
-
-        Arrays.sort(g);
-        Arrays.sort(s);
-
-        int childIndex = 0;
-        int cookieIndex = 0;
-        while (cookieIndex < s.length && childIndex < g.length) {
-
-            if (g[childIndex] <= s[cookieIndex]) 
-	    { 
-		    childIndex++; 
-	    }
-            cookieIndex++;
-        }    
-
-        return childIndex; 
-    }
-
 	
+	   public int findContentChildren2(int[] children, int[] cookies) {
+
+	        Arrays.sort(children);
+	        Arrays.sort(cookies);
+
+	        int childIndex = 0;
+	        int cookieIndex = 0; 
+	        //	Input: g = [1,2], s = [1,2,3] [ g - childrens, s - cookies
+	        //Input: g = [1,2,3], s = [1,1]
+	        while (cookieIndex < cookies.length && childIndex < children.length) {
+
+	            if (children[childIndex] <= cookies[cookieIndex])
+	            { 
+	            	childIndex++;
+	            }
+	            cookieIndex++;
+	        }    
+
+	        return childIndex; 
+	    }
+	   
+	   
 }

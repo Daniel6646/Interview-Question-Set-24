5ec477d9cdd85062d184b2107868684ddacc7eeb
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2026-07-22 20:28:49 +0530

    Update ValidPalindrome2.java

--- Leetcode/ValidPalindrome2.java
+++ Leetcode/ValidPalindrome2.java
@@ -76,4 +76,38 @@ public class ValidPalindrome2 {
 		 
 	    }
 	
+	 public boolean validPalindrome2(String s) {
+
+		    int left = 0;
+		    int right = s.length() - 1;
+
+		    while (left < right) {
+
+		        if (s.charAt(left) == s.charAt(right)) {
+		            left++;
+		            right--;
+		        } else {
+		            return check(s, left + 1, right) ||
+		                   check(s, left, right - 1);
+		        }
+		    }
+
+		    return true;
+		}
+
+		public boolean check(String s, int left, int right) {
+
+		    while (left < right) {
+
+		        if (s.charAt(left) != s.charAt(right))
+		            return false;
+
+		        left++;
+		        right--;
+		    }
+
+		    return true;
+		}
+	 
+	 
 }

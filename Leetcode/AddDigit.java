7d96f89632ba1b67ed0cf5a943211bad00a1faa7
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2026-07-28 12:54:56 +0530

    Update AddDigit.java

--- Leetcode/AddDigit.java
+++ Leetcode/AddDigit.java
@@ -29,6 +29,7 @@ public class AddDigit {
 //
 
 	
+	
 	public static void main(String[] args) {
 		
 		int num1 =38, num2 = 0;
@@ -36,57 +37,61 @@ public class AddDigit {
 		System.out.println("Final output "+result);
 	}
 	
-	
 	public static int addDigit(int num) {
-		
+
 		int digit, sum = 0;
 		boolean flag = false;
-		
-		while(flag != true) {
-		
+
+		while (flag != true) {
+
 //		while (number != 0) {
-//		
 //			digit = number % 10;
 //			sum = sum + digit;
-//			number = number / 10;
-//				
+//			number = number / 10;		
 //		}
-		
-		 sum = calculateSum(num);
-			
-		if(sum < 10) {
-			
-			flag =  true;
-			break;
+
+			sum = calculateSum(num);
+
+			if (sum < 10) {
+
+				flag = true;
+				break;
+				// return sum;
+			}
+
+			else {
+
+				num = sum;
+
+			}
+
 		}
-		
-		else {
-			
-			num = sum;
-			
-		}
-	
-		}
-		
+
 		return sum;
+	}
+
+	public static int addDigits2(int num) {
+
+	    while (num >= 10) {
+	        num = calculateSum(num);
+	    }
+
+	    return num;
 	}
 	
 	
-	public static int calculateSum( int number) {
-		
-		int sum =0, digit =0;
-		
+	public static int calculateSum(int number) {
+
+		int sum = 0, digit = 0;
+
 		while (number != 0) {
-		
+
 			digit = number % 10;
 			sum = sum + digit;
 			number = number / 10;
-				
-			
+
 		}
 		return sum;
 	}
-	
-	
 	
 }

1f345a87e136fc29cd60790fcbcdafc7aee92098
Daniel Q <76101943+Daniel6646@users.noreply.github.com>
2026-07-28 19:00:00 +0530

    Update SingleNumber.java

--- Leetcode/SingleNumber.java
+++ Leetcode/SingleNumber.java
@@ -2,32 +2,33 @@ package LeetCode;
 
 public class SingleNumber {
 
-Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//	Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//
+//	You must implement a solution with a linear runtime complexity and use only constant extra space.
+//
+//	 
+//
+//	Example 1:
+//
+//	Input: nums = [2,2,1]
+//	Output: 1
+//	Example 2:
+//
+//	Input: nums = [4,1,2,1,2]
+//	Output: 4
+//	Example 3:
+//
+//	Input: nums = [1]
+//	Output: 1
+//	 
+//
+//	Constraints:
+//
+//	1 <= nums.length <= 3 * 104
+//	-3 * 104 <= nums[i] <= 3 * 104
+//	Each element in the array appears twice except for one element which appears only once.
 
-You must implement a solution with a linear runtime complexity and use only constant extra space.
-
- 
-
-Example 1:
-
-Input: nums = [2,2,1]
-Output: 1
-Example 2:
-
-Input: nums = [4,1,2,1,2]
-Output: 4
-Example 3:
-
-Input: nums = [1]
-Output: 1
- 
-
-Constraints:
-
-1 <= nums.length <= 3 * 104
--3 * 104 <= nums[i] <= 3 * 104
-Each element in the array appears twice except for one element which appears only once.
-
+	
 	
 	public static void main(String[] args) {
 		
@@ -49,5 +50,16 @@ Each element in the array appears twice except for one element which appears onl
 		  }
         return xor; 
     }
-	
+
+	  public int singleNumber2(int[] nums) {
+
+		    int xor = 0;
+
+		    for (int num : nums) {
+		        xor ^= num;
+		    }
+
+		    return xor;
+		}
+	  
 }
